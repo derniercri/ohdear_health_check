d8fdb9bbd3d8a7aed399e0808f44bf3f9ec735d6
Stephane DE LA TORRE <s.dlt@hotmail.fr>
2022-11-18 15:57:37 +0100

    initial commit

---
+++ lib/ohdear_health_check.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'ohdear_health_check/version'
+
+module OhdearHealthCheck
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ohdear_health_check_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe OhdearHealthCheck do
+  it 'has a version number' do
+    expect(OhdearHealthCheck::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
